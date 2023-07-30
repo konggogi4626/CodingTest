80b76b5fc52ef456d2cbbdc474eac0ce516fead7
konggogi4626 <125466311+konggogi4626@users.noreply.github.com>
2023-07-31 08:58:25 +0900

    [Add] Fast Code

--- Ch3/q2.py
+++ Ch3/q2.py
@@ -14,6 +14,16 @@ def solution(n,m,k,data:list) -> int:
         m-=1
     return sol
 
+def solution_fast(n,m,k,data:list) -> int:
+    data.sort() # 정렬, 마지막 인덱스 
+    first_large = data[n-1]
+    second_large = data[n-2]
+    sol = 0
+    first_large_cnt = int(m/(k+1)) * k
+    first_large_cnt += m%(k+1)
+    sol += first_large*first_large_cnt
+    sol += second_large*(m-first_large_cnt)
+    return sol
 
 
 
@@ -21,4 +31,6 @@ if __name__ == "__main__":
     n, m, k = map(int,input().split())
     data = list(map(int,input().split()))
     answer = solution(n,m,k,data)
+    answer_fast = solution_fast(n,m,k,data)
     print(answer)
+    print(answer_fast)
